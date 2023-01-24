e2e2329e4f1c32271a198754f46938e2baae974c
tsvetomiraD <95402102+tsvetomiraD@users.noreply.github.com>
2023-01-24 20:19:57 +0200

    Add files via upload

---
+++ restApi/src/main/java/rest/api/restApi/SpringbootMybatisCrudExampleApplication.java
@@ -0,0 +1,9 @@
+package rest.api.restApi;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringbootMybatisCrudExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisCrudExampleApplication.class, args);
+    }
+}
